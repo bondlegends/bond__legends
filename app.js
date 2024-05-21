358fe9fd2fe06bc12dfe557016b7cfe181fbda1a
bondlegends <botsetup7@gmail.com>
2024-05-21 17:30:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -81,12 +81,7 @@ let track_list = [
     image: "https://i.ytimg.com/vi/-7n_krPLRgs/0.jpg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
     path: "./ktb/mw.mp3"
   },   
-     {
-    name: "Don - Bae Song",
-    artist: "Sivakarthikeyan, Priyanka Mohan | Anirudh Ravichander",
-    image: "https://i.ytimg.com/vi/-7n_krPLRgs/0.jpg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
-    path: "./ktb/m7.mp3"
-  },   
+      
 ];
 
 /*
